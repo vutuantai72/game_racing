0f0010263dec0b8409a0f111716270547eea0388
vutuantai72 <vutuantai72>
2025-05-03 09:43:29 +0700

    Update code

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
